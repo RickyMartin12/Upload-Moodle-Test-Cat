dc94cef37310d4f578aa894b364588dd44b2f884
RickyMartin12 <ricardopeleira16@gmail.com>
2023-02-24 14:14:39 +0000

    test133300112211222333

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    //phpinfo();
-
+    phpinfo();
+    /*
     $servername = "us-cdbr-east-06.cleardb.net";
 $username = "b590e12a78989c";
 $password = "35b87add";
@@ -10,5 +10,5 @@ $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 if (!$conn) {
     die("Erro ao ligar DB" . mysqli_connect_error());
 }
-
+*/
 ?>
