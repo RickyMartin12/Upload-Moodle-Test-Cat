af26cc08b61f6424ad111cc610bc9d4df5341415
RickyMartin12 <ricardopeleira16@gmail.com>
2023-02-24 13:58:18 +0000

    test1333001122

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    phpinfo();
+    //phpinfo();
+
+    $servername = "us-cdbr-east-06.cleardb.net";
+$username = "b590e12a78989c";
+$password = "35b87add";
+$dbname = "heroku_613ab8d128e91a9";
+$port = "3306";
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+if (!$conn) {
+    die("Erro ao ligar DB" . mysqli_connect_error());
+}
+
 ?>
